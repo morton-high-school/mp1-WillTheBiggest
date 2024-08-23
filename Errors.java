a1ce753562d0a18d3f3e742fe54b25a17f26e727
WillTheBiggest <William.webb@mcusd709.org>
2024-08-22 21:53:29 -0500

    Update Errors.java

--- Errors.java
+++ Errors.java
@@ -3,15 +3,15 @@
 
 Public class Error{
   public static main(string[] args){
-    String name = "Alan"
+    string name = "Alan";
     string animal = "duck";
-    System.outprintln("My name is " + Name + " and I am a " + animal + ".");
+    System.outprint.ln("My name is " + Name + " and I am a " + animal + ".");
 
-    Int weight = 90;
+    int weight = 90;
     int volume = 8;
-    double density = weight/volume; // Should be the correct precise value.
+    double density = (double) weight/volume; // Should be the correct precise value.
 
-    System.out.pintln("I am a heavy " + animal + " because I have a density of " + dense + " kilograms per cubic centimeter.");
+    System.out.println("I am a heavy " + animal + " because I have a density of " + dense + " kilograms per cubic centimeter.");
 
     System.out.println("It was nice meeting you!")
   }
